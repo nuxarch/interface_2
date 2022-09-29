2ee2f242a8e262287a50b061cc8479776bd043fa
nuxarch <misbaqul4sfi4k>
2022-09-29 10:54:55 +0700

    controlling stepper motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
 #include <Arduino.h>
 
-#include <MD_Parola.h>
-#include <MD_MAX72xx.h>
-#include <SPI.h>
-#include "switch.h"
-#include "PinChangeInterrupt.h"
-#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 4
+#define PIN_IN1 10
+#define PIN_IN2 11
+#define PIN_IN3 12
+#define PIN_IN4 13
 
-#define CLK_PIN 13
-#define DATA_PIN 11
-#define CS_PIN 10
+#define PIN1_HIGH   digitalWrite(PIN_IN1, HIGH)
+#define PIN1_LOW   digitalWrite(PIN_IN1, LOW)
+#define PIN2_HIGH   digitalWrite(PIN_IN2, HIGH)
+#define PIN2_LOW   digitalWrite(PIN_IN2, LOW)
+#define PIN3_HIGH   digitalWrite(PIN_IN3, HIGH)
+#define PIN3_LOW   digitalWrite(PIN_IN3, LOW)
+#define PIN4_HIGH   digitalWrite(PIN_IN4, HIGH)
+#define PIN4_LOW   digitalWrite(PIN_IN4, LOW)
 
-// Arbitrary output pins
-MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
-#define PIN_RESPONSIF 50
-int counter = 0;
-char buff[5];
-
-void detect_sensor()
-{
-  delay(100);
-  counter++;
-}
-
-void setup()
-{
+void setup(){
   Serial.begin(115200);
-  konfig_switch();
-  pinMode(PIN_RESPONSIF, INPUT_PULLUP);
-  attachPCINT(digitalPinToPCINT(PIN_RESPONSIF), detect_sensor, FALLING);
-  P.begin();
+  pinMode(PIN_IN1, OUTPUT);
+  pinMode(PIN_IN2, OUTPUT);
+  pinMode(PIN_IN3, OUTPUT);
+  pinMode(PIN_IN4, OUTPUT);
+
 }
+void loop(){
+  // PIN 1234 = 0001, 0010, 0100, 1000
+  for (int i = 0; i < 50; i++)
+  {
+    PIN1_LOW; PIN2_LOW; PIN3_LOW; PIN4_HIGH;delay(10);
+    PIN1_LOW; PIN2_LOW; PIN3_HIGH; PIN4_LOW;delay(10);
+    PIN1_LOW; PIN2_HIGH; PIN3_LOW; PIN4_LOW;delay(10);
+    PIN1_HIGH; PIN2_LOW; PIN3_LOW; PIN4_LOW;delay(10);
+  }
+  // PIN 1234 = 1000, 0100, 0010, 0001
 
-void loop()
-{
-  itoa(counter, buff, 10);
-
-  if (P.displayAnimate())
-    P.displayText(buff, PA_CENTER, 50, 1000, PA_SCROLL_UP, PA_SCROLL_UP);
-  P.setZoneEffect(0, true, PA_FLIP_LR);
-  P.setZoneEffect(0, true, PA_FLIP_UD);
+  PIN1_LOW; PIN2_LOW; PIN3_LOW; PIN4_LOW;delay(1000);
+  for (int i = 0; i < 50; i++)
+  {
+    PIN1_HIGH; PIN2_LOW; PIN3_LOW; PIN4_LOW;delay(10);
+    PIN1_LOW; PIN2_HIGH; PIN3_LOW; PIN4_LOW;delay(10);
+    PIN1_LOW; PIN2_LOW; PIN3_HIGH; PIN4_LOW;delay(10);
+    PIN1_LOW; PIN2_LOW; PIN3_LOW; PIN4_HIGH;delay(10);
+  }
+  PIN1_LOW; PIN2_LOW; PIN3_LOW; PIN4_LOW;delay(1000);
 
 }
